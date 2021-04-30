88ff210ad3e03a9bb8585e90bd23a6419f3ef9ed
Patrick Lai <patricklai14@gmail.com>
2021-04-29 19:04:15 -0500

    Use constant LR for training classifier, run classification training loop 3 times, add CL args

--- classify.py
+++ classify.py
@@ -5,8 +5,10 @@ from transformers import LineByLineTextDataset
 from transformers import RobertaTokenizer, RobertaForSequenceClassification
 from transformers import Trainer, TrainingArguments
 
+import numpy as np
 import torch
 
+import argparse
 import pdb
 
 #guides used:
@@ -27,12 +29,29 @@ def compute_metrics(pred):
     }
 
 def main():
+    parser = argparse.ArgumentParser(description="Run feature selection")
+    parser.add_argument("--train_dataset", type=str, required=True,
+                        help="dataset file to train classifier")
+    parser.add_argument("--val_dataset", type=str, required=True,
+                        help="dataset file for validation data")
+    parser.add_argument("--test_dataset", type=str, required=True,
+                        help="dataset file to evaluate classifier")
+    parser.add_argument("--model", type=str, required=True,
+                        help="location of pretrained model or \"roberta-base\" for base roberta")
+    parser.add_argument("--output_dir", type=str, required=True,
+                        help="directory to save model")
+
+    args = parser.parse_args()
+    args_dict = vars(args)
+
     train_data = datasets.load_dataset(
-                    "json", data_files="D:\\Work\\sandbox\\data\\train_citation.jsonl")["train"]
+                    "json", data_files=args_dict["train_dataset"])["train"]
     
     #not a typo: load_dataset loads this as training data
+    val_data = datasets.load_dataset(
+                    "json", data_files=args_dict["val_dataset"])["train"] 
     test_data = datasets.load_dataset(
-                    "json", data_files="D:\\Work\\sandbox\\data\\test_citation.jsonl")["train"] 
+                    "json", data_files=args_dict["test_dataset"])["train"] 
 
     #assign an integer key for each label
     label_keys = {}
@@ -48,8 +67,6 @@ def main():
             num_labels += 1
 
     tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
-    model = RobertaForSequenceClassification.from_pretrained(".\\roberta_tapt_citation", hidden_dropout_prob=0.1, num_labels=num_labels)
-    #model = RobertaForSequenceClassification.from_pretrained('roberta-base', hidden_dropout_prob=0.1, num_labels=num_labels)
 
     def tokenization(batched_text):
         tokenized_batch = tokenizer(batched_text['text'], padding=True, truncation=True)
@@ -57,36 +74,54 @@ def main():
         return tokenized_batch
 
     train_data = train_data.map(tokenization, batched=True, batch_size=len(train_data))
+    val_data = val_data.map(tokenization, batched=True, batch_size=len(val_data))
     test_data = test_data.map(tokenization, batched=True, batch_size=len(test_data))
 
     train_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
+    val_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
     test_data.set_format('torch', columns=['input_ids', 'attention_mask', 'label'])
 
-    training_args = TrainingArguments(
-        output_dir="./roberta_tapt_citation",
-        overwrite_output_dir=True,
-        num_train_epochs=10,
-        per_device_train_batch_size=4,
-        per_device_eval_batch_size=4,
-        learning_rate=2e-5,
-        fp16=True,
-        eval_accumulation_steps=20,
-        save_steps=5000,
-        save_total_limit=2,
-        seed=3
-    )
+    f1_scores = []
+    for seed in range(1, 4):
+        model = RobertaForSequenceClassification.from_pretrained(
+                args_dict["model"], hidden_dropout_prob=0.1, num_labels=num_labels)
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,
-        compute_metrics=compute_metrics,
-        train_dataset=train_data,
-        eval_dataset=test_data
-    )
+        training_args = TrainingArguments(
+            output_dir=args_dict["output_dir"],
+            overwrite_output_dir=True,
+            num_train_epochs=10,
+            per_device_train_batch_size=4,
+            per_device_eval_batch_size=4,
+            learning_rate=2e-5,
+            lr_scheduler_type="constant",
+            fp16=True,
+            eval_accumulation_steps=20,
+            save_strategy="no",
+            save_steps=5000,
+            save_total_limit=1,
+            load_best_model_at_end=False,
+            metric_for_best_model="f1",
+            evaluation_strategy="epoch",
+            seed=seed
+        )
 
-    trainer.train()
-    print(trainer.evaluate())
-    pdb.set_trace()
+        trainer = Trainer(
+            model=model,
+            args=training_args,
+            compute_metrics=compute_metrics,
+            train_dataset=train_data,
+            eval_dataset=val_data
+        )
+
+        trainer.train()
+
+        metrics = trainer.evaluate(test_data)
+        print("Metrics for seed {}: {}".format(seed, metrics))
+
+        f1_scores.append(metrics["eval_f1"])
+
+    print("Avg f1 score: {}, stdev: {}".format(np.mean(f1_scores), np.std(f1_scores)))
+
 
 if __name__ == "__main__":
     main()
--- pretrain.py
+++ pretrain.py
@@ -13,7 +13,8 @@ def main():
     model = RobertaForMaskedLM.from_pretrained('roberta-base')
 
     #train_data_file = "D:\\Work\\sandbox\\data\\train_citation.jsonl"
-    train_data_file = "D:\\Work\\sandbox\\data\\train_scierc.jsonl"    
+    train_data_file = "D:\\Work\\sandbox\\data\\train_scierc.jsonl"
+    #train_data_file = "D:\\Work\\sandbox\\data\\train_chemprot.jsonl"
     train_data = datasets.load_dataset(
                     "json", data_files=train_data_file)["train"]
 
@@ -30,12 +31,13 @@ def main():
     )
 
     training_args = TrainingArguments(
-        output_dir="./roberta_tapt_scierc",
+        output_dir="./tapt_scierc",
         overwrite_output_dir=True,
         num_train_epochs=100,
         per_device_train_batch_size=2,
         per_device_eval_batch_size=2,
         learning_rate=1e-4,
+        #lr_scheduler_type="constant",        
         adam_epsilon=1e-6,
         adam_beta1=0.9,
         adam_beta2=0.98,
@@ -45,7 +47,7 @@ def main():
         eval_accumulation_steps=20,
         save_steps=5000,
         save_total_limit=2,
-        seed=1
+        seed=2
     )
 
     trainer = Trainer(
@@ -56,7 +58,7 @@ def main():
     )
     trainer.train()
 
-    trainer.save_model("./roberta_tapt_scierc")
+    trainer.save_model("./tapt_scierc")
 
 if __name__ == "__main__":
     main()
